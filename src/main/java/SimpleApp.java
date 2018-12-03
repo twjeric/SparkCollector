b4a1270b4248bd6ed1b3d1cda55673953ed9f16e
wuwenjun <wuwenjun@pku.edu.cn>
2018-12-03 14:30:31 -0800

    Config the system to support the first application.

---
+++ src/main/java/SimpleApp.java
@@ -0,0 +1,21 @@
+import org.apache.spark.api.java.function.FilterFunction;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.Dataset;
+import scala.Function1;
+
+import java.util.Arrays;
+
+public class SimpleApp {
+    public static void main(String[] args) {
+        String logFile = "README.md"; // Should be some file on your system
+        SparkSession spark = SparkSession.builder().appName("Simple Application").getOrCreate();
+        Dataset<String> logData = spark.read().textFile(logFile).cache();
+
+        long numAs = logData.filter((FilterFunction<String>) s -> s.contains("a")).count();
+        long numBs = logData.filter((FilterFunction<String>) s -> s.contains("b")).count();
+
+        System.out.println("Lines with a: " + numAs + ", lines with b: " + numBs);
+
+        spark.stop();
+    }
+}
